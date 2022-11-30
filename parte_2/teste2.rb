312d891f3d967b55527d9ac524b9bb85b22ee106
marianavalias <marivalias2013@gmail.com>
2022-11-30 10:44:18 -0300

    parte_2

---
+++ parte_2/teste2.rb
@@ -0,0 +1,7 @@
+#repeticao de um laco
+# + soma numeros ou contatena string
+#dica, pensar na logica como criança, passo a passo
+
+for dedos in 1..3 #entre os numeros, para cada dedos entre
+    puts "contando " + dedos.to_s #transformando em string 
+end 
